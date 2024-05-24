2e9eaeadf3e624107d8f5ca240123bd3c0fa473a
up2157231 <66753907+up2157231@users.noreply.github.com>
2024-05-24 13:12:11 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:setapprojects/pages/navBar.dart';
+import 'package:setapprojects/pages/user_model.dart';
+import 'pages/firebase_options.dart';
+import 'pages/login.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
+}
